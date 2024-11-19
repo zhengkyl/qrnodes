23fc39824b8ab9453ec49933c7b90b882a266bad
Kyle Zheng <kylezheng73@gmail.com>
2024-11-19 02:31:20 -0500

    simple save/load state

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,52 @@
+import { unwrap } from "solid-js/store";
 import { Panels } from "./components/Panels";
-import { NodesContextProvider } from "./components/context/NodesContext";
+import {
+  NodesContextProvider,
+  useNodesContext,
+} from "./components/context/NodesContext";
+import { batch, createSignal, Show } from "solid-js";
 
-function App() {
+function AppWithContext() {
+  const { nodes, setNodes, setActiveIds } = useNodesContext();
+  const [loaded, setLoaded] = createSignal(true);
   return (
     <div class="flex flex-col h-screen">
       <div class="flex gap-4">
         <h1>qrnodes</h1>
         <a href="https://github.com/zhengkyl/qrnodes">source code</a>
+        <button
+          onClick={() => {
+            navigator.clipboard.writeText(JSON.stringify(unwrap(nodes)));
+          }}
+        >
+          copy state
+        </button>
+        <button
+          onClick={async () => {
+            setLoaded(false);
+            const jsonNodes = await navigator.clipboard.readText();
+            batch(() => {
+              setActiveIds([]);
+              setNodes(JSON.parse(jsonNodes));
+              setLoaded(true);
+            });
+          }}
+        >
+          load state from clipboard
+        </button>
       </div>
-      <NodesContextProvider>
+      <Show when={loaded()}>
         <Panels />
-      </NodesContextProvider>
+      </Show>
     </div>
+  );
+}
+
+function App() {
+  return (
+    <NodesContextProvider>
+      <AppWithContext />
+    </NodesContextProvider>
   );
 }
 
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,3 +1,3 @@
 export function Button(props) {
-  return <button></button>;
+  return <button>{props.children}</button>;
 }
